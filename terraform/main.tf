1d5279294e20ecb923ec6d98c0034324d520e1c2
Caiuã Iorio <91350621+Caiuaeoreo@users.noreply.github.com>
2024-08-10 05:53:37 +0000

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,8 +119,8 @@ resource "aws_security_group" "eks_cluster_sg" {
 
   ingress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
