cf2492ba6249b67ae20afb888f99afe672398af3
Caiuã Iorio <91350621+Caiuaeoreo@users.noreply.github.com>
2024-08-10 04:19:18 +0000

    Update S3 bucket name for Terraform state

--- bucket-tfstate/main.tf
+++ bucket-tfstate/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"
+  bucket = "caiua-localiza-terraform-state-bucket"
   acl    = "private"
   tags = {
     Name = "Terraform State Bucket"
