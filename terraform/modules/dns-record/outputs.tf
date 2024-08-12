85b0ff517ce172349b87927a4cc790ab9b36cb40
Caiuã Iorio <91350621+Caiuaeoreo@users.noreply.github.com>
2024-08-12 07:44:01 +0000

    Localiza-DevOps: Update value of aws_route53_record in outputs.tf

--- terraform/modules/dns-record/outputs.tf
+++ terraform/modules/dns-record/outputs.tf
@@ -5,5 +5,5 @@ output "lb_dns_name" {
 
 output "aws_route53_record" {
   description = "DNS name of the load balancer"
-  value       = aws_route53_record.flask_app_dns.name
+  value       = aws_route53_record.flask_app_dns.fqdn
 }
