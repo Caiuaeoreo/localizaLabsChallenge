bc3638ebc1939075dc3dae25641a92fcada383de
Caiuã Iorio <91350621+Caiuaeoreo@users.noreply.github.com>
2024-08-12 07:47:30 +0000

    Localiza-DevOps: Removing output name of LoadBalancer DNS

--- terraform/modules/dns-record/outputs.tf
+++ terraform/modules/dns-record/outputs.tf
@@ -1,8 +1,3 @@
-output "lb_dns_name" {
-  description = "DNS name of the load balancer"
-  value       = aws_lb.my_flask_app_lb.dns_name
-}
-
 output "aws_route53_record" {
   description = "DNS name of the load balancer"
   value       = aws_route53_record.flask_app_dns.fqdn
