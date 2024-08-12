f570b3f3af869f838844db132178baafa8e412b0
Caiuaeoreo <caiuak@gmail.com>
2024-08-12 01:54:37 -0300

    Localiza-DevOps: blank line in end of code

--- flask-app/test_app.py
+++ flask-app/test_app.py
@@ -1,19 +1,22 @@
 from app import app
 import pytest
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 def test_hello(client):
     response = client.get("/")
     assert response.status_code == 200  # nosec
     assert response.data == b"Hello Localiza Labs!"  # nosec
 
+
 def test_ping(client):
     response = client.get("/ping")
     assert response.status_code == 200  # nosec
     assert response.data == b"pong"  # nosec
 
-# Adiciona uma nova linha ao final do arquivo
+# End of code
